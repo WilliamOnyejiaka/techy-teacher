11b68a42cb49e3dc93aa212fc70821c6449c2f90
WilliamOnyejiaka <58482913+WilliamOnyejiaka@users.noreply.github.com>
2022-01-30 10:19:28 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from src.api.v1.models.user import User
 from src.api.v1.models.bookmarks import Bookmark
 
 app = create_app()
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 
